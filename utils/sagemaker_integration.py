f2de24600a7f0235728ec45cf895015c1003b3d4
ketanGangal <ketangangal98@gmail.com>
2022-02-27 15:34:16 +0530

    endpoint deletion function added

--- utils/sagemaker_integration.py
+++ utils/sagemaker_integration.py
@@ -45,7 +45,7 @@ def deploy_model_aws_sagemaker(config=None):
         return f"Error Occurred while Deploying : {e.__str__()} "
 
 
-def query(input_json, config):
+def query(input_json, config=None):
     try:
         app_name = config['params']['app_name']
         region = config['params']['region']
@@ -62,12 +62,42 @@ def query(input_json, config):
         return f"Error Occurred While Prediction : {e.__str__()}"
 
 
-def switching_models():
-    pass
+def switching_models(config=None):
+    try:
+        app_name = config['params']['app_name']
+        execution_role_arn = config['params']['execution_role_arn']
+        image_ecr_url = config['params']['image_ecr_url']
+        region = config['params']['region']
+        s3_bucket_name = config['params']['s3_bucket_name']
+        experiment_id = config['params']['experiment_id']
+        new_run_id = config['params']['run_id']
+        model_name = config['params']['model_name']
+
+        new_model_uri = "s3://{}/{}/{}/artifacts/{}/".format(s3_bucket_name, experiment_id, new_run_id, model_name)
+
+        response = mfs.deploy(app_name=app_name,
+                              model_uri=new_model_uri,
+                              execution_role_arn=execution_role_arn,
+                              region_name=region,
+                              image_url=image_ecr_url,
+                              mode=mfs.DEPLOYMENT_MODE_REPLACE)
+
+        return f"Model Successfully Changed : {new_run_id}"
+
+    except Exception as e:
+        return f"Error While Changing Model : {e.__str__()}"
 
 
-def remove_deployed_model():
-    pass
+def remove_deployed_model(config=None):
+    try:
+        app_name = config['params']['app_name']
+        region = config['params']['region']
+
+        mfs.delete(app_name=app_name, region_name=region)
+        return f"Endpoint Successfully Deleted : {app_name}"
+
+    except Exception as e:
+        return f"Error While Deleting EndPoint : {e.__str__()}"
 
 
 if __name__ == "__main__":
