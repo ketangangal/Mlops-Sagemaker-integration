67a53d2ad5f1e7d1ae6b71c93ad8f252541149a9
ketanGangal <ketangangal98@gmail.com>
2022-03-12 19:29:40 +0530

    function update

--- aws_production_control/delete_endpoint.py
+++ aws_production_control/delete_endpoint.py
@@ -1,4 +1,4 @@
-from utils.sagemaker_integration import remove_deployed_model
+from utils.sagemaker_integration import sagemaker_integration
 from utils.common import read_config
 
 # Instructions
@@ -13,6 +13,7 @@ from utils.common import read_config
 
 
 if __name__ == "__main__":
-    config = read_config("configurations/config.yaml")
-    response = remove_deployed_model(config)
+    config = read_config("aws_configurations/config.yaml")
+    sagemaker = sagemaker_integration(config)
+    response = sagemaker.remove_deployed_model()
     print(response)
--- aws_production_control/prediction.py
+++ aws_production_control/prediction.py
@@ -1,12 +1,16 @@
 from utils.common import read_config
+from utils.sagemaker_integration import sagemaker_integration
 import pandas as pd
 
-config = read_config('configurations/config.yaml')
-data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual sugar":{"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur dioxide":{"0":34},"density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4}}')
-input_json = data.to_json(orient='split')
 
-Response = query(input_json, config)
+data = pd.read_json('{"fixed acidity":{"0":7.4},"volatile acidity":{"0":0.7},"citric acid":{"0":0},"residual sugar":{'
+                    '"0":1.9},"chlorides":{"0":0.076},"free sulfur dioxide":{"0":11},"total sulfur dioxide":{"0":34},'
+                    '"density":{"0":0.9978},"pH":{"0":3.51},"sulphates":{"0":0.56},"alcohol":{"0":9.4}}')
 
 if __name__ == "__main__":
+    config = read_config('aws_configurations/config.yaml')
+    input_json = data.to_json(orient='split')
+    sagemaker = sagemaker_integration(config)
+    Response = sagemaker.query(input_json)
     print(f"Predictions From Model EndPoint : {Response}")
 
