f7632d58e9f893845ad6fbd77ffc7124489d6961
ketanGangal <ketangangal98@gmail.com>
2022-03-12 19:30:15 +0530

    added read_json and update yaml

--- utils/common.py
+++ utils/common.py
@@ -1,4 +1,7 @@
 import yaml
+import json
+from from_root import from_root
+import os
 
 
 def read_config(config_path):
@@ -6,3 +9,24 @@ def read_config(config_path):
         content = yaml.safe_load(config_file)
 
     return content
+
+
+def read_json(path):
+    json_file = open(path)
+    content = json.load(json_file)
+    json_file.close()
+
+    return content
+
+
+def update_config(config_path, data):
+    with open(config_path, 'w') as config_file:
+        config_file.write(yaml.dump(data, default_flow_style=False))
+
+    return "Done"
+
+
+if __name__ == '__main__':
+    path = os.path.join(from_root(), 'aws_infrastructure/output.json')
+    content = read_json(path)
+    database_endpoint = content['values']['root_module']['resources'][0]['values']['address']
